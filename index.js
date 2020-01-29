0c1a9a1a8584b4944bd98bca79d8d558aac69559
Ryan Ma <ryanyma@umich.edu>
2020-01-28 19:03:32 -0500

    added validation and register mutation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 import path from 'path';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+import cors from 'cors';
 import models from './models';
 
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')), { all: true });
@@ -15,6 +16,7 @@ const schema = makeExecutableSchema({
 });
 
 const app = express();
+app.use(cors('*'));
 
 const graphqlEndpoint = '/graphql';
 
--- resolvers/user.js
+++ resolvers/user.js
@@ -1,3 +1,14 @@
+import bcrypt from 'bcrypt';
+import _ from 'lodash';
+
+const formatErrors = (e, models) => {
+  if (e instanceof models.Sequelize.ValidationError) {
+    //  _.pick({a: 1, b: 2}, 'a') => {a: 1}
+    return e.errors.map((x) => _.pick(x, ['path', 'message']));
+  }
+  return [{ path: 'name', message: 'Non-validation error went wrong.' }];
+};
+
 export default {
   Query: {
     getUser: (parent, { id }, { models }) => models.User.findOne({
@@ -8,6 +19,20 @@ export default {
     allUsers: (parent, args, { models }) => models.User.findAll(),
   },
   Mutation: {
-    createUser: (parent, args, { models }) => models.User.create(args),
+    register: async (parent, { password, ...otherArgs }, { models }) => {
+      try {
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const user = await models.User.create({ ...otherArgs, password: hashedPassword });
+        return {
+          ok: true,
+          user,
+        };
+      } catch (err) {
+        return {
+          ok: false,
+          errors: formatErrors(err, models),
+        };
+      }
+    },
   },
 };
